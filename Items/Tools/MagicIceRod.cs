ff27a4cae2a2f801827b8278ad4db01394e6aec6
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2022-05-13 18:55:25 +0800

    Moving files to better locations

---
+++ Items/Tools/MagicIceRod.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace MemeClasses.Items
+{
+    internal class MagicIceRod : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.IceRod);
+        }
+    }
+}
